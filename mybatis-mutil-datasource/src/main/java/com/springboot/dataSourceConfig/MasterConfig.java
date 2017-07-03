b4fb1c9305940d6400e2b7e57c1ede0419f923b6
wanglu-jf <password>
2017-07-03 17:11:56 +0800

    20170703code

--- mybatis-mutil-datasource/src/main/java/com/springboot/dataSourceConfig/MasterConfig.java
+++ mybatis-mutil-datasource/src/main/java/com/springboot/dataSourceConfig/MasterConfig.java
@@ -16,6 +16,13 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import javax.sql.DataSource;
 
 /**
+ * @Primary 标志这个 Bean 如果在多个同类 Bean 候选时，该 Bean 优先被考虑。「多数据源配置的时候注意，必须要有一个主数据源，用 @Primary 标志该 Bean」
+ * @MapperScan 扫描 Mapper 接口并容器管理，包路径精确到 master，为了和下面 cluster 数据源做到精确区分
+ * @Value 获取全局配置文件 application.properties 的 kv 配置,并自动装配
+ * sqlSessionFactoryRef 表示定义了 key ，表示一个唯一 SqlSessionFactory 实例
+ *
+ * 上面数据配置分别扫描 Mapper 接口，org.spring.springboot.dao.master（对应 xml classpath:mapper/master ） 和 org.spring.springboot.dao.cluster（对应 xml classpath:mapper/cluster ） 包中对应的 UserDAO 和 CityDAO 。
+ * 都有 @Mapper 标志为 Mybatis 的并通过容器管理的 Bean。Mybatis 内部会使用反射机制运行去解析相应 SQL。
  * Created by wanglu-jf on 17/6/28.
  */
 @Configuration
